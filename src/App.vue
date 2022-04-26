4730d6aaa16eb1db1e9eebadfaef271dbb0a3431
Lorenzo Ariatta <lorenzotk77@gmail.com>
2022-04-26 15:32:26 +0200

    header component

--- src/App.vue
+++ src/App.vue
@@ -16,12 +16,9 @@ export default {
 </script>
 
 <style lang="scss">
+@import "@/style/general.scss";
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
 }
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,52 @@
 <template>
-  <div></div>
+  <div class="container-fluid py-4">
+    <div class="la-container">
+      <div class="row align-items-center justify-content-around">
+        <div class="col-5 d-flex">
+          <nav>
+            <ul>
+              <li>
+                <a href="#">Home</a>
+              </li>
+              <li>
+                <a href="#">Pages</a>
+              </li>
+              <li>
+                <a href="#">Courses</a>
+              </li>
+              <li>
+                <a href="#">Features</a>
+              </li>
+              <li>
+                <a href="#">Blog</a>
+              </li>
+              <li>
+                <a href="#">Shop</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div class="col-2 d-flex">
+          <div class="logo">
+            <img src="@/assets/dark-logo.png" alt="logo" class="w-100" />
+          </div>
+        </div>
+        <div class="col-5 d-flex justify-content-end">
+          <div class="icons d-flex align-items-center">
+            <div class="cart me-4">
+              <i class="fa-solid fa-cart-shopping"></i>
+            </div>
+            <div class="user me-4">
+              <i class="fa-regular fa-circle-user"></i>
+            </div>
+            <div class="search-wrapper">
+              <input type="text" placeholder="  Search..." />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -11,4 +58,46 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+.la-container {
+  width: 80%;
+  margin: 0 auto;
+}
+ul {
+  margin: 0%;
+}
+li {
+  list-style-type: none;
+  display: inline;
+  margin-right: 2rem;
+  & a {
+    text-decoration: none;
+    color: #08427d;
+    font-weight: 500;
+  }
+}
+li:after {
+  font-family: "FontAwesome";
+  content: "\f078";
+  color: #08427d;
+  font-size: 8px;
+  margin-left: 5px;
+}
+.search-wrapper {
+  display: inline-block;
+  position: relative;
+  & input {
+    background-color: #e1e1e1;
+    border: none;
+    border-radius: 5px;
+    padding: 5px;
+  }
+}
+.search-wrapper::after {
+  font-family: "FontAwesome";
+  content: "\f002";
+  color: #1fad95;
+  position: absolute;
+  right: 6px;
+  top: 6px;
+}
 </style>
