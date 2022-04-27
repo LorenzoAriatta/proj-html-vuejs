ba7ccb9d442ea795e8e51e1a31d5ebafd7fcd3b0
Lorenzo Ariatta <lorenzotk77@gmail.com>
2022-04-27 19:27:55 +0200

    init footer

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <Header />
     <JumboComponent />
     <MainCompenent />
+    <FooterComp />
   </div>
 </template>
 
@@ -10,6 +11,7 @@
 import Header from "@/components/Header.vue";
 import JumboComponent from "@/components/JumboComponent.vue";
 import MainCompenent from "@/components/MainComponent.vue";
+import FooterComp from "@/components/Footer.vue";
 
 export default {
   name: "App",
@@ -17,6 +19,7 @@ export default {
     Header,
     JumboComponent,
     MainCompenent,
+    FooterComp,
   },
 };
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,74 @@
+<template>
+  <footer class="container-fluid py-5">
+    <div class="la-container">
+      <div class="row">
+        <div class="col-6">
+          <h5>Address</h5>
+          <p>382 NE 191st St # Miami, FL 33179-3899</p>
+          <p>+1(305) 547-9909 (9am - 5pm EST, Monday - Friday)</p>
+          <p>support@maxcoach.com</p>
+          <div class="social">
+            <div class="icon d-inline me-4">
+              <a href="#"><i class="fa-brands fa-twitter"></i></a>
+            </div>
+            <div class="icon d-inline me-4">
+              <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
+            </div>
+            <div class="icon d-inline me-4">
+              <a href="#"><i class="fa-brands fa-instagram"></i></a>
+            </div>
+            <div class="icon d-inline me-4">
+              <a href="#"><i class="fa-brands fa-linkedin"></i></a>
+            </div>
+          </div>
+        </div>
+        <div class="col-6 links">
+          <div class="explore d-flex flex-column flex-wrap">
+            <ul>
+              <h5>Explore</h5>
+              <li><a href="#">Star here</a></li>
+              <li><a href="#">Blog</a></li>
+              <li><a href="#">About Us</a></li>
+              <li><a href="#">Success story</a></li>
+              <li><a href="#">Courses</a></li>
+              <li><a href="#">Contact Us</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "FooterComp",
+};
+</script>
+
+<style scoped lang="scss">
+footer {
+  background-color: #141423;
+}
+.la-container {
+  width: 65%;
+  margin: 0 auto;
+}
+h5 {
+  color: white;
+}
+p {
+  color: #b1b1b0;
+  font-size: 14px;
+}
+a {
+  color: #b1b1b0;
+  font-size: 26px;
+}
+li {
+  list-style-type: none;
+  & a {
+    text-decoration: none;
+  }
+}
+</style>
